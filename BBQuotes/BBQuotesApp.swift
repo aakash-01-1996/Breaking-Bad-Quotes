508b32e68789da72b9faf30be4b03ed943ea9ef8
aakash-01-1996 <aakashambodkar99@gmail.com>
2025-02-04 22:37:44 -0700

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Aakash Ambodkar on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
